18e9862ff520876d5d77d1150a6340dc3205b067
ryuska <yromanova2010@gmail.com>
2019-04-29 15:38:02 -0400

    First Commit

---
+++ src/com/company/Car.java
@@ -0,0 +1,64 @@
+package com.company;
+
+public class Car {
+    private String make;
+    private String model;
+    private String speed;
+    private String color;
+    private String emission;
+
+
+
+    //    constructor
+    public Car(){
+        System.out.println("A new car!");
+
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(String speed) {
+        this.speed = speed;
+    }
+
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String a) {
+        this.color = a;
+    }
+
+    public void start() {
+        System.out.println( "The "+ getColor() + " " + getMake() + " " + getModel() + " is starting");
+    }
+    public String accelerate() {
+        return " The "+ getColor() + " " + getMake() + " " + getModel() + " is accelerating";
+    }
+    public String getMph() {
+        return "The "+ getColor() + " " + getMake() + " " + getModel() + "  is going" + " " + getSpeed() + " mph";
+    }
+    public String stop() {
+        return "The"+ getColor() + " " + getMake() + " " + getModel() + " has stopped";
+
+    }
+}
---
+++ src/com/company/CarApp.java
@@ -0,0 +1,45 @@
+package com.company;
+
+
+
+public class CarApp {
+    public static void main (String[] args){
+        Car a = new Car();
+        a.setMake("Porshe");
+        a.setModel("CV");
+        a.setSpeed("90");
+        a.setColor("Red");
+        a.start();
+        System.out.println(a.accelerate());
+        System.out.println(a.getMph());
+        System.out.println(a.stop());
+        System.out.println(a.getEmission());
+
+        Car b = new Car();
+        b.setMake("Jetta");
+        b.setModel("X");
+        b.setSpeed("30");
+        b.setColor("blue");
+        b.start();
+        System.out.println(b.accelerate());
+        System.out.println(b.getMph());
+        System.out.println(b.stop());
+//        print(a.accelerate());
+//        print(a.go());
+//        print(a.stop());
+//
+//        Porshe b = new Porshe();
+//        print(b.start());
+//        print(b.accelerate());
+//        print(a.go());
+//
+
+
+
+    }
+    private static void print(String s){
+        System.out.println(s);
+    }
+
+
+}
