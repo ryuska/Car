024f719ab9be6a840d2652fdcb2adecc3edebb17
ryuska <yromanova2010@gmail.com>
2019-05-02 10:02:27 -0400

    Final

--- src/com/company/CarApp.java
+++ src/com/company/CarApp.java
@@ -7,8 +7,10 @@ import java.util.Scanner;
 public class CarApp {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-//  ArrayList<Car> vroom = new ArrayList<>();
-        Car a = new Car();
+        ArrayList<Car> cars = new ArrayList<>();
+        int i;
+        for (i = 0; i < 3; i++) {
+            Car a = new Car();
 //        a.setMake("Porshe");
 //        a.setModel("CV");
 //        a.setSpeed("90");
@@ -17,21 +19,30 @@ public class CarApp {
 //        System.out.println(a.accelerate());
 //        System.out.println(a.getMph());
 //        System.out.println(a.stop());
-        System.out.println("Please enter your car type? ");
-        String type = input.next();
-        a.setMake(type);
-        System.out.println("Please enter your car model? ");
-        String model = input.next();
-        a.setModel(model);
-        System.out.println("What is your car speed? ");
-        String speed = input.next();
-        a.setSpeed(speed);
-        System.out.println("What is color of your car? ");
-        String color = input.next();
-        a.setColor(color);
+            System.out.println("Please enter your car type? ");
+            String type = input.next();
+            a.setMake(type);
+            System.out.println("Please enter your car model? ");
+            String model = input.next();
+            a.setModel(model);
+            System.out.println("What is your car speed? ");
+            String speed = input.next();
+            a.setSpeed(speed);
+            System.out.println("What is color of your car? ");
+            String color = input.next();
+            a.setColor(color);
+            cars.add(a);
 
+            for (Car x : cars) {
+//            System.out.println(x.getMake());
+//            System.out.println(x.getModel());
+//            System.out.println(x.getSpeed());
+//            System.out.println(x.getColor());
+                System.out.println(x.getDescription());
 
-//        addCars(a, vroom);
+            }
+        }
+
 
 /*
         Car b = new Car();
@@ -48,18 +59,16 @@ public class CarApp {
         */
 //        addCars(b, vroom);
 
-        ArrayList<Car> cars = new ArrayList<>();
-        cars.add(a);
+//        ArrayList<Car> cars = new ArrayList<>();
+//        cars.add(a) {
 
-        for (Car x : cars) {
-//            System.out.println(x.getMake());
-//            System.out.println(x.getModel());
-//            System.out.println(x.getSpeed());
-//            System.out.println(x.getColor());
-            System.out.println(x.getDescription());
-        }
     }
 }
+
+
+
+
+
 
 
 
